c9ab86e905b861a10b702de1f48e2901fca3624b
gracelove <govlmo91@gmail.com>
2019-11-08 01:51:04 +0900

    init

---
+++ src/main/java/com/petcare/web/service/SampleService.java
@@ -0,0 +1,22 @@
+package com.petcare.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.petcare.web.mapper.TimeMapper;
+
+@Service
+public class SampleService {
+
+
+    @Autowired
+    private TimeMapper timeMapper;
+
+    public String getName() {
+        return "grace";
+    }
+
+    public String now() {
+        return timeMapper.getTime();
+    }
+}
