8d536514869b598a51902ce91d9a76c392d63c10
hannncrystal <hannnsj0108@gmail.com>
2019-11-25 04:48:07 +0900

    Hospital 백엔드 기능 추가

--- src/main/java/com/petcare/web/controller/HospitalController.java
+++ src/main/java/com/petcare/web/controller/HospitalController.java
@@ -1,10 +1,14 @@
 package com.petcare.web.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.petcare.web.domain.HospitalVO;
 import com.petcare.web.service.HospitalService;
 
 @Controller
@@ -15,21 +19,26 @@ public class HospitalController {
 	private HospitalService hospitalService;
 	
 	@GetMapping("/list")
-	public String hospitalList() {
-		hospitalService.list();
+	public String hospitalList(Model model) {
+		ArrayList<HospitalVO> list = hospitalService.list();
+		model.addAttribute("list",list);
+		//all? attribute?
 		return "hospitalList";
-	}//model 객체?
+	}
 	
 	@GetMapping("/search")
-	public String hospitalSearch(String hospitalName) {
-		hospitalService.search(hospitalName);
+	public String hospitalSearch(Model model, String hospitalName) {
+		ArrayList<HospitalVO> search = hospitalService.search(hospitalName);
+		model.addAttribute("search",search);
+		//all? attribute?
 		return "hospitalList";
-	}//model 객체?
+	}
 	
-	@GetMapping("/list")
-	public String hospitalView(String hospitalId) {
-		hospitalService.view(hospitalId);
+	@GetMapping("/view")
+	public String hospitalView(Model model, String hospitalId) {
+		HospitalVO view = hospitalService.view(hospitalId);
+		model.addAttribute("view",view);
 		return "hospitalView";
-	}//model 객체?
+	}
 	
 }
--- src/main/webapp/WEB-INF/views/hospitalView.jsp
+++ src/main/webapp/WEB-INF/views/hospitalView.jsp
@@ -8,39 +8,25 @@
 <title>병원 보기</title>
 </head>
 <body>
-
-<div>
-	<h1>동물병원 목록</h1>
-	<form method="get" action="hospitalsearch">
-		<select>
-		  <option value="name">병원 이름</option>
-		  <option value="address">병원 주소</option>
-		</select>
-		<input type="text" name="text" placeholder="검색"><br>
-		  <input type="checkbox" name="choice1" value="nightcare">야간진료
-		  <input type="checkbox" name="choice2" value="emergency">응급실
-		  <input type="checkbox" name="choice3" value="special">특수동물
-		  <input type="checkbox" name="choice4" value="operation">수술전문
-		<input type="submit" value="검색">
-	</form>
-	
+<!-- 지도 -->
+<!-- 예약하기, 즐겨찾기 버튼 -->
+<!-- 리뷰 붙이기 -->
 	<ul>
-		<c:forEach items="${list}" var="hospital">
-			${hospital.hospitalid }<br>
-			${hospital.hospitalnum }<br>
-			${hospital.opentime }<br>
-			${hospital.closetime }<br>
-			${hospital.address }<br>
-			<c:forEach items="${boolean}">
-				${boolean.nightcare }<br>
-				${boolean.emergency }<br>
-				${boolean.special }<br>
-				${boolean.operation }<br>
-			</c:forEach>
+		<c:forEach items="${view}" var="list">
+			${list.hospitalName}<br>
+			${list.hospitalAddress}<br>
+			${list.hospitalPhone}<br>
+			${list.hospitalEmail}<br>
+			${list.hospitalOpen}<br>
+			${list.hospitalClose}<br>
+			${list.hospitalNightcare}<br>
+			${list.hospitalEmergency}<br>
+			${list.hospitalSpecial}<br>
+			${list.hospitalHotel}<br>
+			${list.hospitalSpecies}<br>
+			${list.hospitalPhoto}<br>
+			${list.hospitalIntro}<br>
 		</c:forEach>
 	</ul>
-	
-</div>
-
 </body>
 </html>
