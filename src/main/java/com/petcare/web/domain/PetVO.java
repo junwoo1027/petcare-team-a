bf910f3d1f2d933c4355fbc2ee3173eb46f6cec9
ghd075 <rlfls54@gmail.com>
2019-11-26 01:00:33 +0900

    Revert "충돌 해결!!"
    This reverts commit 2eb1e552ba46f50e9c01c4e8b9677f74e2d1a83b.

--- src/main/java/com/petcare/web/domain/PetVO.java
+++ src/main/java/com/petcare/web/domain/PetVO.java
@@ -1,33 +1,23 @@
 package com.petcare.web.domain;
 
-import java.util.Date;
-
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.Data;
 
-//동물vo
 @Data
-public class PetVo {
+public class PetVO {
 
-	private int petNo; //동물번호
-	@NotEmpty(message ="이름을 입력해주세요.")
-	private String petName; //동물이름
-	private String petGender; //동물성별
-	private String petNeuter; //중성화
-	@Min(value=1, message="숫자를 입력해주세요.")
-	private int petWeight; //동물 몸무게
-	@NotEmpty(message = "동물 종류를 입력해주세요.")
-	private String petSpecies; //동물 종류
-	@NotEmpty(message = "동물 품종을 입력해주세요.")
-	private String petBreed; //품종
-	@Min(value=1, message="숫자를 입력해주세요.")
-	private int petAge; //동물 나이
-	private MultipartFile petPhoto; //동물사진
-	private Date petRegdate; //등록일
-	private String userId;//동물주인
+	//예약할떄 동물 불렁는 것 때문에만든 것, 
+	
+	private String petNo;
+	private String petName;
+	private String petGender;
+	private String petNeuter;
+	private String petWeight;
+	private String petSpecies;
+	private String petBreed;
+	private String petAge;
+	private String petPhoto;
+	private String userId;
+	private String regDate;
+	
 	
 }
